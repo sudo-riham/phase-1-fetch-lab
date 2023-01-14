0d4b7b896c7fe27009e097f5a5b236ff8bc8e4c9
Riham Zeidan <64324442+sudo-riham@users.noreply.github.com>
2023-01-14 18:07:46 +0200

    edited

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fetchBooks() {
+https://github.com/sudo-riham/phase-1-fetch-lab.gitfunction fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => { return resp.json() })
+    // .then((json) => console.log(json))
+    .then((json) => { return renderBooks(json) })
 }
 
 function renderBooks(books) {
